f6985a7dbd72141ade2d78dbc9f545f0e3b5e950
xuanchienueh <xuanchien.ueh@gmail.com>
2022-03-17 14:04:15 +0700

    test

--- src/index.js
+++ src/index.js
@@ -25,6 +25,12 @@ app.get("/", (req, res) => {
 app.get("/news", (req, res) => {
   res.render("news");
 });
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+app.post("/search", (req, res) => {
+  res.send("search");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
